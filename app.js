55b3ad11942d61122e63282bc1ab1f523aba5918
mattroth <me@mattroth.com>
2024-05-02 17:00:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ const ffmpeg = childProcess.spawn(
         '-update', '1',
         '-y', './public/images/ocr-frame.png',
     ],
-    //{stdio: ['ignore', process.stdout, process.stderr]}
+    {stdio: [process.stdin, process.stdout, process.stderr]}
     //{detached: true}
 );
 
